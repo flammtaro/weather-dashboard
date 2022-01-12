0196e8b1c7031d1caa929833edf890b2e0e56cd3
Reid Somers Flamm <reidsomersflamm@gmail.com>
2022-01-11 17:29:14 -0800

    Finished HTML framework and most of css

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-var requestUrl = 'api.openweathermap.org/data/2.5/weather';
 
 var APIKey = "6071a91aca35893504af7b8555d6a497";
 
-var city;
+var city = ["chicago"];
 var state;
 var country;
+
+var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6071a91aca35893504af7b8555d6a497";
 
 function getApi(requestUrl) {
     fetch(requestUrl)
